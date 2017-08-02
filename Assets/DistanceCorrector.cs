e95c3325bb2c7331a6db3c7ba720fb7c21af616d
Ian Burnette <ian_burnette@yahoo.com>
2017-08-02 08:15:45 -0400

    fixed that bug
    fixed that bug

---
+++ Assets/DistanceCorrector.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceCorrector : MonoBehaviour {
+
+    #region Private Variables
+    [SerializeField] float newDist, oldDist;
+#endregion
+
+#region Public Properties
+
+#endregion
+
+#region Unity Functions
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.name == "player_PT")
+        {
+            oldDist = other.GetComponent<PlayerSwitch>().MinDistance;
+            other.GetComponent<PlayerSwitch>().MinDistance = newDist;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.name == "player_PT")
+        {
+            other.GetComponent<PlayerSwitch>().MinDistance = oldDist;
+        }
+    }
+    #endregion
+
+    #region Custom Functions
+
+    #endregion
+}
